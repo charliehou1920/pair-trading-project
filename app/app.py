b28abaa02c992d21b025a98ca0b3d2a8fa23347c
NorthSeaPark <hou.308@osu.edu>
2023-12-16 14:25:09 -0500

    update

--- app/app.py
+++ app/app.py
@@ -31,7 +31,7 @@ def main():
     st.image("../image/OPTICS_Clustering.png")
 
     # Brief Summary of The Project
-    st.write("This project implements a pairs trading strategy using machine learning methods. There are two parts for this project. In the first part of the project, we used unsupervised learning techniques like OPTICS clustering and statistical methods such as Engle-Granger test to filter out the qualified pairs of stocks for trading. In the second part, A LSTM model was implemented to generate buy and sell signals for trading. We built a basktesting system to evaluate the returns of our pair trading strategy and compared it with the benchmark of S&P 500 index")
+    st.write("This project implements a pairs trading strategy using machine learning methods. There are two parts for this project. In the first part of the project, we used unsupervised learning techniques like OPTICS clustering and statistical methods such as Engle-Granger test to filter out the qualified pairs of stocks for trading. In the second part, A LSTM model was implemented to generate buy and sell signals for trading. We built a basktesting system to evaluate the returns of our pair trading strategy and compared it with the benchmark of S&P 500 index.")
 
     # Retrieve each pair of stock from the results
     pairs = [result['pair'] for result in results]
